7b5ed8db15e8a99d11ed24446660df43f8620c6b
Alex Patterson <alex@codingcat.dev>
2026-01-15 13:05:58 -0500

    init

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import { SanityLive } from '@/sanity/lib/live'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SanityLive />
+
       </body>
     </html>
   );
