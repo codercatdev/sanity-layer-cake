7ddd36dfdebb24ee4f56ed1f1f7f909d530a45a9
Alex Patterson <alex@codingcat.dev>
2026-01-15 13:48:10 -0500

    build up the blog

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -1,12 +1,9 @@
-import Link from "next/link";
-import { client } from "@/sanity/lib/client";
-import { POSTS_QUERY } from "@/sanity/lib/queries";
-import { sanityFetch } from "@/sanity/lib/live";
-
-const options = { next: { revalidate: 60 } };
+import Link from 'next/link'
+import { sanityFetch } from '@/sanity/lib/live'
+import { POSTS_QUERY } from '@/sanity/lib/queries'
 
 export default async function Page() {
-    const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
+    const { data: posts } = await sanityFetch({ query: POSTS_QUERY })
 
     return (
         <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
@@ -26,5 +23,5 @@ export default async function Page() {
             <hr />
             <Link href="/">&larr; Return home</Link>
         </main>
-    );
+    )
 }
---
+++ src/sanity/types.ts
@@ -0,0 +1,2 @@
+// Re-export types from the root sanity.types.ts file
+export * from '../../sanity.types';
